899ab1b65f6b7e58c39b5900ee7dab8f23bfd088
Justin Cano <jcano001@ucr.edu>
2014-10-23 20:35:27 -0700

    added route for index

--- sudoku_solver/urls.py
+++ sudoku_solver/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('',
     # url(r'^$', 'sudoku_solver.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', include('sudoku.urls',name='index')),
+    url(r'^$', include('sudoku.urls')),
 )
