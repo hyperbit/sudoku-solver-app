58a4e63310c8c1d7288b919f8738ff4139b47ef1
Justin Cano <jcano001@ucr.edu>
2014-10-23 21:20:01 -0700

    added static file style.css for sudoku app

--- sudoku_solver/settings.py
+++ sudoku_solver/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sudoku'
+    'django.contrib.webdesign',
+    'sudoku',
 )
 
 MIDDLEWARE_CLASSES = (
